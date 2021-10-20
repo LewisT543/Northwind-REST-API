9ef729865350e08f258ad5fd86808d2ca8307b1b
TeamPapaya <34426841+edmundackah@users.noreply.github.com>
2021-10-20 19:01:04 +0100

    implemented endpoints for orders

--- src/main/java/com/sparta/lt/northwindrest/controllers/NorthwindController.java
+++ src/main/java/com/sparta/lt/northwindrest/controllers/NorthwindController.java
@@ -1,13 +1,17 @@
 package com.sparta.lt.northwindrest.controllers;
 
 import com.sparta.lt.northwindrest.entities.CustomerEntity;
+import com.sparta.lt.northwindrest.entities.OrderEntity;
 import com.sparta.lt.northwindrest.entities.ProductEntity;
 import com.sparta.lt.northwindrest.repositories.CustomerRepository;
+import com.sparta.lt.northwindrest.repositories.OrderRepository;
 import com.sparta.lt.northwindrest.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -15,11 +19,14 @@ import java.util.stream.Collectors;
 public class NorthwindController {
     private final ProductRepository productRepository;
     private final CustomerRepository customerRepository;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public NorthwindController(CustomerRepository customerRepository, ProductRepository productRepository) {
+    public NorthwindController(CustomerRepository customerRepository, ProductRepository productRepository,
+                               OrderRepository orderRepository) {
         this.productRepository = productRepository;
         this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
     }
 
     @GetMapping("/products")
@@ -42,5 +49,35 @@ public class NorthwindController {
     @GetMapping("/products/{id}")
     public Optional<ProductEntity> getProductsById(@PathVariable Integer id) {
         return productRepository.findById(id);
+    }
+
+
+    //<-------- Orders Mapping starts here ------->
+    @GetMapping("/orders")
+    public List<OrderEntity> getOrders() {
+        return new OrderController(orderRepository).getOrders();
+    }
+
+    @GetMapping("orders/{orderID}")
+    public Optional<OrderEntity> getOrderByID(@PathVariable Integer orderID) {
+        return new OrderController(orderRepository).getOrderByID(orderID);
+    }
+
+    @GetMapping("orders/customer/{customerID}")
+    public List<OrderEntity> getOrdersByCustomerID(@PathVariable String customerID) {
+        return new OrderController(orderRepository).getOrdersCustomerID(customerID);
+    }
+
+    @GetMapping("orders/location")
+    public List<OrderEntity> getOrdersByRegionAndCountry(
+            @RequestParam String country, @RequestParam(required = false) String region) {
+        return new OrderController(orderRepository).getOrdersByCountryAndRegion(country, region);
+    }
+
+    @GetMapping("orders/date")
+    public List<OrderEntity> getOrdersByOrderDateAndShippedDate(
+            @RequestParam String orderDate, @RequestParam(required = false) String shippedDate) {
+        return new OrderController(orderRepository).getOrdersByDate(orderDate, shippedDate);
+
     }
 }
--- src/main/java/com/sparta/lt/northwindrest/controllers/OrderController.java
+++ src/main/java/com/sparta/lt/northwindrest/controllers/OrderController.java
@@ -1,4 +1,77 @@
 package com.sparta.lt.northwindrest.controllers;
 
+
+import com.sparta.lt.northwindrest.entities.OrderEntity;
+import com.sparta.lt.northwindrest.repositories.OrderRepository;
+import com.sparta.lt.northwindrest.util.Util;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class OrderController {
+    private OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+    public List<OrderEntity> getOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<OrderEntity> getOrderByID(Integer orderID) {
+        return orderRepository.findById(orderID);
+    }
+
+    public List<OrderEntity> getOrdersCustomerID(String customerID) {
+        return orderRepository.findAll()
+                .stream()
+                .filter((o) -> o.getCustomerID().contains(customerID))
+                .collect(Collectors.toList());
+    }
+
+    //TODO: Null handling
+    public List<OrderEntity> getOrdersByCountryAndRegion(String country, String region) {
+        if (country != null && region != null) {
+            List<OrderEntity> results = new ArrayList<>();
+            for (OrderEntity o : orderRepository.findAll()) {
+                if (o.getShipCountry() != null && o.getShipRegion() != null) {
+                    if (o.getShipCountry().contains(country) && o.getShipRegion().contains(region)) {
+                        results.add(o);
+                    }
+                }
+            }
+            return results;
+        } else if (region == null) {
+            return orderRepository.findAll()
+                    .stream()
+                    .filter((o) -> o.getShipCountry().contains(country))
+                    .collect(Collectors.toList());
+        } else return null;
+    }
+
+    public List<OrderEntity> getOrdersByDate(String orderDate, String shippedDate) {
+        if (orderDate != null && shippedDate != null) {
+            List<OrderEntity> results = new ArrayList<>();
+            for (OrderEntity o: orderRepository.findAll()) {
+                if (o.getOrderDate() != null && o.getShippedDate() != null) {
+                    if(o.getOrderDate().equals(Util.getDateAsInstant(orderDate))
+                            && o.getShippedDate().equals(Util.getDateAsInstant(shippedDate))) {
+                        results.add(o);
+                    }
+                }
+            }
+            return results;
+        } else if (shippedDate == null) {
+            return orderRepository.findAll()
+                    .stream()
+                    .filter((o) -> o.getOrderDate().equals(Util.getDateAsInstant(orderDate)))
+                    .collect(Collectors.toList());
+        } else return null;
+    }
 }
